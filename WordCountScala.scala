a837fba109a5f70137a3d829e20fd459b0fdd0e2
Vivvi <57920864+vivschmidt@users.noreply.github.com>
2020-05-29 10:34:40 +0200

    Update WordCountScala.scala

--- WordCountScala.scala
+++ WordCountScala.scala
@@ -2,3 +2,6 @@ val input = sc.textFile("input.txt")
 val count = input.flatMap(line => line.split (" ")) 
 .map(word => (word,1)) 
 .reduceByKey(_+_)
+
+val words = input.flatMap(line => line.split(" "))
+.map(word => (word))
